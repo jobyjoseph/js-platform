6a60f32b0e1cd7003d295ef76b262211d3117839
Joby Joseph <reachjoby@gmail.com>
2017-05-29 14:55:18 +0530

    on module 13 - demo toggle mock api over

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,24 +1,21 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import compression from 'compression';
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
 
 /*eslint-disable no-console */
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+// enable gzip
+app.use(compression());
 
+app.use(express.static('dist'));
 
 // Handling routes
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.get('/users', function(req, res) {
